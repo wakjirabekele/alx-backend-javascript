7ebde744314a573832acdb6bc89465d4761d49d8
Wakjirabekele <wakjirabekele2018@gmail.com>
2023-09-25 13:51:51 +0000

    my commit

--- 10-loops.js
+++
@@ -1,9 +0,0 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array;
-  for (const value of array) {
-    const idx = array.indexOf(value);
-    newArray[idx] = appendString + value;
-  }
-
-  return newArray;
-}
--- 11-createEmployeesObject.js
+++
@@ -1,4 +0,0 @@
-export default function createEmployeesObject(departmentName, employees) {
-  const employee = { [departmentName]: employees };
-  return employee;
-}
