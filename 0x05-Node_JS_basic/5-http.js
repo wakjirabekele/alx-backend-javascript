856bef83f6e9d52a59a569fe7068c0d9d049bc9e
Wakjira Bekele <73541693+wakjirabekele@users.noreply.github.com>
2023-11-21 15:14:21 +0300

    Update 5-http.js

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,37 +1,29 @@
-/* Create a small HTTP server using the http module:
-It should be assigned to the variable app and this one must be exported
-HTTP server should listen on port 1245
-It should return plain text
-When the URL path is /,
-it should display Hello Holberton School! in the page body
-When the URL path is /students,
-it should display This is the list of our students
-followed by the same content as the file 3-read_file_async.js
-(with and without the database)
-the name of the database must be passed as argument of the file
-CSV file can contain empty lines (at the end)
-and they are not a valid student!
- */
-
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const hostname = '127.0.0.1';
 const port = 1245;
-const fileName = process.argv[2];
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    countStudents(fileName).then((data) => {
-      data.unshift('This is the list of our students');
-      res.end(data.join('\n'));
-    }).catch((error) => {
-      res.end(`${error}`);
-    });
+    let dbInfo = 'This is the list of our students\n';
+    await countStudents(process.argv[2])
+      .then((msg) => {
+        dbInfo += msg;
+        res.end(dbInfo);
+      })
+      .catch((err) => {
+        dbInfo += err.message;
+        res.end(dbInfo);
+      });
   }
 });
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
 
 module.exports = app;
